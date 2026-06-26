4eb9d71c1aed5c0fc29ebded7843524ca3a9dc15
Matt Higham <mhigham@stlawu.edu>
2026-06-26 10:52:33 -0400

    trying to see if model works after scaling.

--- bayesian_logistic.R
+++ bayesian_logistic.R
@@ -18,31 +18,40 @@ subset_games <- final_data |>
   #filter(time_elapsed %% 24 == 0)
 
 # scale time and score so algorithm runs better
+
+min_lead_diff <- min(subset_games$lead_diff)
+max_lead_diff <- max(subset_games$lead_diff)
+
+min_rating_diff <- min(subset_games$rating_diff)
+max_rating_diff <- max(subset_games$rating_diff)
+
+## puts all predictors on the [0, 1] scale
 subset_games <- subset_games |>
   mutate(
-    time_scaled = time_elapsed / 2400,
-    lead_scaled = lead_diff / 50,
-    ratings_scaled = rating_diff / 50
+    time_scaled = (time_elapsed - 0) / (2400 - 0),
+    lead_scaled = (lead_diff - min_lead_diff) / (max_lead_diff - min_lead_diff),
+    rating_scaled = (rating_diff - min_rating_diff) / (max_rating_diff - min_rating_diff)
   )
 
 
 # Model
-brm_mod_rank <- brm(home_win ~ lead_diff + time_elapsed + rating_diff + time_elapsed:lead_diff + time_elapsed:rating_diff,
+brm_mod_rank <- brm(home_win ~ lead_scaled + time_scaled + rating_scaled + time_scaled:lead_scaled + time_scaled:rating_scaled,
                     data = subset_games,
                     family = bernoulli(),
-                    prior = c(prior(normal(0, 2), class = "Intercept"))
+                    prior = c(prior(normal(0, 4), class = "Intercept"))
 )
 
 
 # Plot
-# model predictions
-preds_brm_mod_rank <- ggpredict(brm_mod_rank, terms = c("time_elapsed [0:2400 by=10]", 
-                                                        "lead_diff [-15, -10, -5, 0, 5, 10, 15]")) |>
-  as.data.frame(terms_to_colnames = TRUE) |>
-  as_tibble()
+# model predictions: will need to change the values to correspond
+# with scaled versions of the predictors.
+# preds_brm_mod_rank <- ggpredict(brm_mod_rank, terms = c("time_elapsed [0:2400 by=10]", 
+#                                                         "lead_diff [-15, -10, -5, 0, 5, 10, 15]")) |>
+#   as.data.frame(terms_to_colnames = TRUE) |>
+#   as_tibble()
 
 ##  -- Save rds file --
-saveRDS(preds_brm_mod_rank, file = "bayesian_logistic_full.rds")
+saveRDS(brm_mod_rank, file = "bayesian_logistic_full.rds")
 
 
 
